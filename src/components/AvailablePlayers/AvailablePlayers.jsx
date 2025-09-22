401716e6b71952bd6db246e382198fd49b27c11d
Samiul-Alam-Shanto <samiulalamshantowpdev@gmail.com>
2025-09-22 10:23:54 +0600

    added the fake json data with the navbar component

---
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AvailablePlayers = () => {
+  return <div></div>;
+};
+
+export default AvailablePlayers;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+import coinImg from "../../assets/dollar.png";
+
+const Navbar = () => {
+  return (
+    <div class="navbar bg-base-100 shadow-sm max-w-[1220px] mx-auto">
+      <div class="flex-1">
+        <a class=" text-xl">
+          <img className="w-[60px] h-[60px]" src={logo} alt="" />
+        </a>
+      </div>
+      <div class="flex gap-4  items-center">
+        <div>
+          <ol className="flex gap-4 text-gray-600">
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Fixture</a>
+            </li>
+            <li>
+              <a href="#">Teams</a>
+            </li>
+            <li>
+              <a href="#">Schedules</a>
+            </li>
+          </ol>
+        </div>
+        <div class="flex gap-1 btn items-center">
+          <span>6000000000</span>
+          <span>Coins</span>
+          <img className="w-[20px] h-[20px]" src={coinImg} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
